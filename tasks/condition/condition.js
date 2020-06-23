d9dd5c0e8ec834b951543e5876274bb95bc197f6
Tatsiana.Makarevich <Tatsiana_Makarevich@epam.com>
2020-06-23 09:16:16 +0300

    conditions_cycles_hometask

--- tasks/condition/condition.js
+++ tasks/condition/condition.js
@@ -5,14 +5,11 @@
  */
 {
   const a = 3,
-        b = 2;
+    b = 2;
 
-  if (a + b < 4) {
-    console.log('wrong');
-  } else {
-    console.log('too much');
-  }
+  (a + b < 4) ? console.log('wrong'): console.log('too much');
 }
+
 
 /** TODO
  * The system receives two params x and y.
@@ -21,8 +18,20 @@
  * Task 2 - ternary operator
  */
 {
-    const x = 3;
-    const y = 7;
+  const x = 10;
+  const y = 2;
+
+  if (x < 10 && y < 10) {
+    z = x + y;
+  } else if (x >= 10 || y >= 10) {
+    /**I select || operator for cases when one of the variables is equal to 10, and the other is less than 10 */
+    z = x * y;
+  }
+  console.log(z);
+
+  (x < 10 && y < 10) ? console.log(z = x + y):
+    (x >= 10 || y >= 10) ? console.log(z = x * y) : '';
+  /**How can I 'close' ternarny operator if there is no code for the 'else' parameter? */
 }
 
 /** TODO
@@ -32,7 +41,26 @@
  */
 
 {
-    const x = 3;
-    const y = 7;
-    const operator = "add";
+  const x = 35;
+  const y = 7;
+  const operator = 'divide';
+  switch (operator) {
+    case 'add':
+      console.log(z = x + y);
+      break;
+    case 'substract':
+      console.log(z = x - y);
+      break;
+    case 'multiply':
+      console.log(z = x * y);
+      break;
+    case 'divide':
+      console.log(z = x / y);
+      break;
+    default:
+      console.log('Default behaviour');
+      break;
+  }
+
+
 }
--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -9,20 +9,50 @@
  * @param {number} redemption amount (0 - 9999)
  * @returns {number} discount in percent
  */
-
-function calculateDiscount(redemption) {
+{
+  /**What does it mean 'write the function'? */
+  const redemption = 2701;
   let discount;
-
-  //PLACE YOUR CODE HERE
-
-  return discount;
+  if (redemption <= 350) {
+    discount = 0;
+  } else if (redemption > 350 && redemption <= 1350) {
+    discount = 15;
+  } else if (redemption > 1350 && redemption <= 2700) {
+    discount = 30;
+  } else if (redemption > 2700 && redemption <= 6500) {
+    discount = 45;
+  }
+  console.log(discount + '%');
 }
 
 /** TODO
  * implement factorial algorithm using for, while, do..while operators
  */
 {
-  const i = 10; //10! = 3628800
+  const i = 9; //10! = 3628800
+
+  let z = 1;
+  for (let n = 1; n <= i; n++) {
+    z = z * n;
+  }
+  console.log(z);
+
+  let n = 1;
+  let y = 1;
+  while (n <= i) {
+    y = y * n;
+    n++;
+  }
+  console.log(y);
+
+  let a = 1;
+  let b = 1;
+  do {
+    b = b * a;
+    a++;
+  }
+  while (a <= i);
+  console.log(b);
 }
 
 /**
@@ -30,6 +60,11 @@ function calculateDiscount(redemption) {
  */
 {
   const substr = ["I", " love", " JS"];
+  let str = ' '; /**How can I define empty string without displaying of null/undefined and without 'space'? */
+  for (i = 0; i < substr.length; i++) {
+    str = str + substr[i];
+  }
+  console.log(str);
 }
 
 /**
@@ -42,6 +77,10 @@ function calculateDiscount(redemption) {
     interestOnDeposit: 250,
     otherExpences: -300
   };
-}
 
-module.exports = calculateDiscount;
+  let total = 0;
+  for (let type in personIncomes) {
+    total = total + personIncomes[type];
+  }
+  console.log(total);
+}
