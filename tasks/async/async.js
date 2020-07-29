51c7d82b63644caaed12f07b8ea3f2b4e1fea289
Tatsiana.Makarevich <Tatsiana_Makarevich@epam.com>
2020-07-29 16:44:47 +0300

    fixes_async_hometask

--- tasks/async/async.js
+++ tasks/async/async.js
@@ -19,60 +19,47 @@ function printDataCallback() {
  * console.log data got from promiseFunction
  */
 function printDataPromise() {
-    return new Promise(function (resolve, reject) {
-        resolve((promiseFunction()));
-    });
+    promiseFunction().then(result => console.log(result));
 }
-let promise = printDataPromise();
-promise
-    .then(result => console.log(result));
 
 /**
  * console.log data got from promiseFunction using async/await
  */
 async function printDataAsyncAwait() {
-    let promise2 = new Promise(function (resolve, reject) {
-        resolve((promiseFunction()));
-    });
-    let result1 = await promise2;
-    console.log(result1);
+    const result = await promiseFunction();
+    console.log(result);
 }
-printDataAsyncAwait();
 
 /**
  * throw error data from callbackFunctionError
  */
 function handleErrorCallback() {
-    function handleError(str, data) {
-        if (str) throw str;
-        console.log(str);
+    function throwError(err, data) {
+        if (err) throw err;
     }
-    callbackFunctionError(handleError);
+    callbackFunctionError(throwError);
 }
 
 /**
  * throw error come from promiseFunctionError
  */
 function handlePromiseError() {
-    return new Promise(function (resolve, reject) {
-        throw ((promiseFunctionError()));
-    });
+    promiseFunctionError.catch(error => console.log(error));
+
 }
-let promise1 = handlePromiseError();
-promise1
-    .catch(error => console.log(error.message));
+
 
 /**
  * throw error come from promiseFunctionError using async/await
  */
 async function handleAsyncAwaitError() {
-    let promise3 = new Promise(function (resolve, reject) {
-        reject((promiseFunctionError()));
-    });
-    let result2 = await promise3;
-    console.log(result2);
+    try {
+        await promiseFunctionError;
+    } catch (error) {
+        console.log(error);
+    }
 }
-handleAsyncAwaitError();
+
 
 module.exports = {
     printDataCallback,
